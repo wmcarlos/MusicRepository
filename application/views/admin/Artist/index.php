ddc2861a298925d6d8e60504b5230c627c84bb76
Carlos Vargas <carlos-vargas2011@hotmail.com>
2017-10-26 23:28:34 +0200

    add icons in title from modules

--- application/views/admin/Artist/index.php
+++ application/views/admin/Artist/index.php
@@ -3,7 +3,7 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-lg-12">
-                        <h1 class="page-header"><?= $title ?></h1>
+                        <h1 class="page-header"><i class="fa fa-users"></i> <?= $title ?></h1>
                     </div>
                     <!-- /.col-lg-12 -->
                 </div>
